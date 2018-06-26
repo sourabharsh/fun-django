36ca3b26e68ca6d596a0cf14169e727a9cfcdac0
sourabh <sourabharsh@gmail.com>
2018-06-26 15:01:24 +0530

    added all the file under fun_web/*  in git

--- fun_web/urls.py
+++ fun_web/urls.py
@@ -19,6 +19,5 @@ from django.contrib.auth  import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('change_password/', auth_views.PasswordChangeView.as_view())
-
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
